09e86b8eb5aa1043b84ded1078cd81bcf81d01c7
duck <duck@duck.duck>
2015-12-22 17:37:02 +0500

    Added nav menu animation only for switching between main and projects sections, for now.

--- vcard/static/vcard/script.js
+++ vcard/static/vcard/script.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors:['main', 'projects', 'about', 'contacts'],
+        anchors:['main', 'projects', 'about', 'contact'],
         verticalCentered: false,
-        menu: '#nav'
+        onLeave: function(index, nextIndex, direction) {
+            var nav = $('nav');
+
+            if (index == 1 && nextIndex == 2) {
+                $(nav).animate({'bottom': '-50px'}, 700);
+            }
+
+            if (index == 2 && nextIndex == 1) {
+                $(nav).animate({'bottom': '0'}, 700);
+            }
+        }
     });
     $.fn.fullpage.setAllowScrolling(false);
     $.fn.fullpage.setKeyboardScrolling(false);
