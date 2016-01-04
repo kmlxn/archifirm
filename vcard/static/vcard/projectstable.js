df5925cf654ff69f87cfe0cb42a06014a0cabc3d
duck <duck@duck.duck>
2016-01-04 17:52:05 +0500

    Change leading tabs to spaces and add trailing lf to file end.
    In imported libraries and copied code from internet.

--- vcard/static/vcard/projectstable.js
+++ vcard/static/vcard/projectstable.js
@@ -34,213 +34,213 @@
 
     var init = function() {
 
-    	camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
-    	camera.position.z = 3000;
+        camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+        camera.position.z = 3000;
 
-    	scene = new THREE.Scene();
+        scene = new THREE.Scene();
 
-    	// table
+        // table
 
-    	for (var i = 0; i < projects.length; i++) {
+        for (var i = 0; i < projects.length; i++) {
 
-    		var element = document.createElement('div');
-    		element.className = 'element';
+            var element = document.createElement('div');
+            element.className = 'element';
 
-    		var symbol = document.createElement('div');
+            var symbol = document.createElement('div');
             symbol.className = 'picture';
-    		symbol.innerHTML =
+            symbol.innerHTML =
                 '<a href="' + projects[i].url + '">\
                     <img src="' + projects[i].pictureUrl + '">\
                 </a>';
-    		element.appendChild(symbol);
+            element.appendChild(symbol);
 
-    		var details = document.createElement('div');
+            var details = document.createElement('div');
             details.className = 'caption';
-    		details.innerHTML = projects[i].caption;
-    		element.appendChild(details);
+            details.innerHTML = projects[i].caption;
+            element.appendChild(details);
 
-    		var object = new THREE.CSS3DObject(element);
+            var object = new THREE.CSS3DObject(element);
             object.position.x = Math.random() * 4000 - 2000;
             object.position.y = Math.random() * 4000 - 2000;
-    		object.position.z = Math.random() * 4000 - 2000;
-    		scene.add(object);
+            object.position.z = Math.random() * 4000 - 2000;
+            scene.add(object);
 
-    		objects.push(object);
+            objects.push(object);
 
-    		//
+            //
 
-    		var object = new THREE.Object3D();
+            var object = new THREE.Object3D();
             object.position.x = (i % 7 * 220) - 660;
             object.position.y = - (Math.floor(i / 7) * 220);
 
-    		targets.table.push(object);
+            targets.table.push(object);
 
-    	}
+        }
 
-    	// sphere
+        // sphere
 
-    	var vector = new THREE.Vector3();
+        var vector = new THREE.Vector3();
 
-    	for (var i = 0, l = objects.length; i < l; i ++) {
+        for (var i = 0, l = objects.length; i < l; i ++) {
 
-    		var phi = Math.acos(-1 + (2 * i) / l);
-    		var theta = Math.sqrt(l * Math.PI) * phi;
+            var phi = Math.acos(-1 + (2 * i) / l);
+            var theta = Math.sqrt(l * Math.PI) * phi;
 
-    		var object = new THREE.Object3D();
+            var object = new THREE.Object3D();
 
-    		object.position.x = 800 * Math.cos(theta) * Math.sin(phi);
-    		object.position.y = 800 * Math.sin(theta) * Math.sin(phi);
-    		object.position.z = 800 * Math.cos(phi);
+            object.position.x = 800 * Math.cos(theta) * Math.sin(phi);
+            object.position.y = 800 * Math.sin(theta) * Math.sin(phi);
+            object.position.z = 800 * Math.cos(phi);
 
-    		vector.copy(object.position).multiplyScalar(2);
+            vector.copy(object.position).multiplyScalar(2);
 
-    		object.lookAt(vector);
+            object.lookAt(vector);
 
-    		targets.sphere.push(object);
+            targets.sphere.push(object);
 
-    	}
+        }
 
-    	// helix
+        // helix
 
-    	var vector = new THREE.Vector3();
+        var vector = new THREE.Vector3();
 
-    	for (var i = 0, l = objects.length; i < l; i ++) {
+        for (var i = 0, l = objects.length; i < l; i ++) {
 
-    		var phi = i * 0.175 + Math.PI;
+            var phi = i * 0.175 + Math.PI;
 
-    		var object = new THREE.Object3D();
+            var object = new THREE.Object3D();
 
             object.position.x = 1200 * Math.sin(phi);
             object.position.y = - (i * 8);
             object.position.z = 1200 * Math.cos(phi);
 
-    		vector.x = object.position.x * 2;
-    		vector.y = object.position.y;
-    		vector.z = object.position.z * 2;
+            vector.x = object.position.x * 2;
+            vector.y = object.position.y;
+            vector.z = object.position.z * 2;
 
-    		object.lookAt(vector);
+            object.lookAt(vector);
 
-    		targets.helix.push(object);
+            targets.helix.push(object);
 
-    	}
+        }
 
-    	// grid
+        // grid
 
-    	for (var i = 0; i < objects.length; i ++) {
+        for (var i = 0; i < objects.length; i ++) {
 
-    		var object = new THREE.Object3D();
+            var object = new THREE.Object3D();
 
-    		object.position.x = ((i % 5) * 400) - 800;
-    		object.position.y = (- (Math.floor(i / 5) % 5) * 400) + 800;
-    		object.position.z = (Math.floor(i / 25)) * 1000;
+            object.position.x = ((i % 5) * 400) - 800;
+            object.position.y = (- (Math.floor(i / 5) % 5) * 400) + 800;
+            object.position.z = (Math.floor(i / 25)) * 1000;
 
-    		targets.grid.push(object);
+            targets.grid.push(object);
 
-    	}
+        }
 
-    	//
+        //
 
-    	renderer = new THREE.CSS3DRenderer();
-    	renderer.setSize(window.innerWidth, window.innerHeight);
-    	renderer.domElement.style.position = 'absolute';
-    	document.getElementById(parameters.targetContainerId).appendChild(renderer.domElement);
+        renderer = new THREE.CSS3DRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.domElement.style.position = 'absolute';
+        document.getElementById(parameters.targetContainerId).appendChild(renderer.domElement);
 
-    	//
+        //
 
-    	controls = new THREE.TrackballControls(camera, renderer.domElement);
-    	controls.rotateSpeed = 1;
-    	controls.minDistance = 500;
-    	controls.maxDistance = 6000;
-    	controls.addEventListener('change', render);
+        controls = new THREE.TrackballControls(camera, renderer.domElement);
+        controls.rotateSpeed = 1;
+        controls.minDistance = 500;
+        controls.maxDistance = 6000;
+        controls.addEventListener('change', render);
 
-    	var button = document.getElementById(parameters.controlButtonsIds.transformToTable);
-    	button.addEventListener('click', function (event) {
+        var button = document.getElementById(parameters.controlButtonsIds.transformToTable);
+        button.addEventListener('click', function (event) {
 
-    		transform(targets.table, 2000);
+            transform(targets.table, 2000);
 
-    	}, false);
+        }, false);
 
-    	var button = document.getElementById(parameters.controlButtonsIds.transformToSphere);
-    	button.addEventListener('click', function (event) {
+        var button = document.getElementById(parameters.controlButtonsIds.transformToSphere);
+        button.addEventListener('click', function (event) {
 
-    		transform(targets.sphere, 2000);
+            transform(targets.sphere, 2000);
 
-    	}, false);
+        }, false);
 
-    	var button = document.getElementById(parameters.controlButtonsIds.transformToHelix);
-    	button.addEventListener('click', function (event) {
+        var button = document.getElementById(parameters.controlButtonsIds.transformToHelix);
+        button.addEventListener('click', function (event) {
 
-    		transform(targets.helix, 2000);
+            transform(targets.helix, 2000);
 
-    	}, false);
+        }, false);
 
-    	var button = document.getElementById(parameters.controlButtonsIds.transformToGrid);
-    	button.addEventListener('click', function (event) {
+        var button = document.getElementById(parameters.controlButtonsIds.transformToGrid);
+        button.addEventListener('click', function (event) {
 
-    		transform(targets.grid, 2000);
+            transform(targets.grid, 2000);
 
-    	}, false);
+        }, false);
 
-    	transform(targets.table, 2000);
+        transform(targets.table, 2000);
 
-    	//
+        //
 
-    	window.addEventListener('resize', onWindowResize, false);
+        window.addEventListener('resize', onWindowResize, false);
 
     }
 
     var transform = function(targets, duration) {
 
-    	TWEEN.removeAll();
+        TWEEN.removeAll();
 
-    	for (var i = 0; i < objects.length; i ++) {
+        for (var i = 0; i < objects.length; i ++) {
 
-    		var object = objects[ i ];
-    		var target = targets[ i ];
+            var object = objects[ i ];
+            var target = targets[ i ];
 
-    		new TWEEN.Tween(object.position)
-    			.to({ x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration)
-    			.easing(TWEEN.Easing.Exponential.InOut)
-    			.start();
+            new TWEEN.Tween(object.position)
+                .to({ x: target.position.x, y: target.position.y, z: target.position.z }, Math.random() * duration + duration)
+                .easing(TWEEN.Easing.Exponential.InOut)
+                .start();
 
-    		new TWEEN.Tween(object.rotation)
-    			.to({ x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration)
-    			.easing(TWEEN.Easing.Exponential.InOut)
-    			.start();
+            new TWEEN.Tween(object.rotation)
+                .to({ x: target.rotation.x, y: target.rotation.y, z: target.rotation.z }, Math.random() * duration + duration)
+                .easing(TWEEN.Easing.Exponential.InOut)
+                .start();
 
-    	}
+        }
 
-    	new TWEEN.Tween(this)
-    		.to({}, duration * 2)
-    		.onUpdate(render)
-    		.start();
+        new TWEEN.Tween(this)
+            .to({}, duration * 2)
+            .onUpdate(render)
+            .start();
 
     }
 
     var onWindowResize = function() {
 
-    	camera.aspect = window.innerWidth / window.innerHeight;
-    	camera.updateProjectionMatrix();
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
 
-    	renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(window.innerWidth, window.innerHeight);
 
-    	render();
+        render();
 
     }
 
     var animate = function() {
 
-    	requestAnimationFrame(animate);
+        requestAnimationFrame(animate);
 
-    	TWEEN.update();
+        TWEEN.update();
 
-    	controls.update();
+        controls.update();
 
     }
 
     var render = function() {
 
-    	renderer.render(scene, camera);
+        renderer.render(scene, camera);
 
     }
 }())
