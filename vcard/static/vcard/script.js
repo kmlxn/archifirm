016509047493a05d444281e3e03173ae17a8c80d
duck <duck@duck.duck>
2016-01-06 16:55:09 +0500

    Refactor script.js. Code split to functions.

--- vcard/static/vcard/script.js
+++ vcard/static/vcard/script.js
@@ -1,52 +1,74 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-        anchors:['hello', 'we'],
-        verticalCentered: false,
-        controlArrows: false,
-        onLeave: function(index, nextIndex, direction) {
-            var nav = $('.index_page nav');
-
-            if (index == 1 && nextIndex == 2) {
-                $(nav).animate({'bottom': '-50px'}, 700);
-                window.runProjectsTableOnce(window.projects);
-                $('.projectstable .element .picture a').magnificPopup({
-                    type: 'ajax',
-                    callbacks: {
-                        ajaxContentAdded: handlePicturesSelectInProjectDetails
-                    }
-                });
-            }
-
-            if (index == 2 && nextIndex == 1) {
-                $(nav).animate({'bottom': '0'}, 700);
-            }
-        }
-    });
-
-    $.fn.fullpage.setAllowScrolling(false);
-    $.fn.fullpage.setKeyboardScrolling(false);
-
-    $(window).on('hashchange', function() {
-        if (window.location.hash == '#hello')
-            removeHash();
-    });
-
-    var removeHash = function() {
-        var loc = window.location;
-
-        if ('pushState' in history)
-            history.pushState('', document.title, loc.pathname + loc.search);
-        else {
-            loc.hash = '';
-        }
-    };
-
     var handlePicturesSelectInProjectDetails = function() {
-        $('#project_details #additional_pictures a').click(function(e){
+        $('#project_details #additional_pictures a').click(function(e) {
             $('#project_details #picture_preview img').attr('src', $(this).attr('href'));
             $(this).addClass('active');
             $(this).siblings().removeClass('active');
             e.preventDefault();
         });
     };
+
+    var setProjectDetailsPopup = function() {
+        $('.projectstable .element .picture a').magnificPopup({
+            type: 'ajax',
+            callbacks: {
+                ajaxContentAdded: handlePicturesSelectInProjectDetails
+            }
+        });
+    };
+
+    var handleProjectsGridStart = function() {
+        window.runProjectsTableOnce(window.projects);
+        setProjectDetailsPopup()
+    };
+
+    var startFullpage = function() {
+        $('#fullpage').fullpage({
+            anchors: ['hello', 'we'],
+            verticalCentered: false,
+            controlArrows: false,
+            onLeave: function(index, nextIndex, direction) {
+                var nav = $('.index_page nav');
+
+                if (index == 1 && nextIndex == 2) {
+                    $(nav).animate({'bottom': '-50px'}, 700);
+                    handleProjectsGridStart();
+                }
+
+                if (index == 2 && nextIndex == 1) {
+                    $(nav).animate({'bottom': '0'}, 700);
+                }
+            }
+        });
+    };
+
+    var autoRemoveMainSectionHash = function() {
+        $(window).on('hashchange', function() {
+            if (window.location.hash == '#hello')
+            removeHash();
+        });
+
+        var removeHash = function() {
+            var loc = window.location;
+
+            if ('pushState' in history)
+                history.pushState('', document.title, loc.pathname + loc.search);
+            else {
+                loc.hash = '';
+            }
+        };
+    };
+
+    var disableManualScrolling = function() {
+        $.fn.fullpage.setAllowScrolling(false);
+        $.fn.fullpage.setKeyboardScrolling(false);
+    };
+
+    var handleFullpage = function() {
+        startFullpage();
+        disableManualScrolling();
+        autoRemoveMainSectionHash();
+    };
+
+    handleFullpage();
 });
